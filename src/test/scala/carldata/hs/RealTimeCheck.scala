dc4326ba058780c8c1d8cfff790820316f550f0c
wenta <jakub@carlsolutions.com>
2017-08-07 15:12:56 +0200

    Add genScript.

--- src/test/scala/carldata/hs/RealTimeCheck.scala
+++ src/test/scala/carldata/hs/RealTimeCheck.scala
@@ -4,13 +4,20 @@ import spray.json._
 import carldata.hs.RealTime.RealTimeRecord
 import carldata.hs.RealTime.RealTimeJsonProtocol._
 import org.scalacheck.Prop.forAll
-import org.scalacheck.{Gen, Properties}
+import org.scalacheck.{Arbitrary, Gen, Properties}
 
 object RealTimeCheck extends Properties("RealTime") {
+  
+  val genScript = for {
+    spec <- Gen.oneOf(Seq('\n', '\t', ' '))
+    unicode <- Arbitrary.arbString.arbitrary
+    str <- Gen.alphaNumStr
+  } yield List(spec, unicode, str).mkString
+
   private val realtimeRecordGen = for {
     action <- Gen.identifier
     calculation <- Gen.identifier
-    script <- Gen.identifier
+    script <- genScript
     trigger <- Gen.identifier
     outputChannel <- Gen.identifier
   } yield RealTimeRecord(action, calculation, script, trigger, outputChannel)
