3854ff92af9f07770391bf0cca29b6929ac43a31
sharpcoding <sharp.coding@gmail.com>
2017-08-18 10:29:22 +0200

    Issue #12 - implementing code review remarks

--- src/test/scala/carldata/hs/avro/EventBusRecordStatusAvro.java
+++ src/test/scala/carldata/hs/avro/EventBusRecordStatusAvro.java
@@ -7,7 +7,7 @@ package carldata.hs.avro;
 @SuppressWarnings("all")
 @org.apache.avro.specific.AvroGenerated
 public enum EventBusRecordStatusAvro {
-  BatchCalculationStarted, BatchCalculationStopped, Unkown  ;
-  public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"enum\",\"name\":\"EventBusRecordStatusAvro\",\"namespace\":\"carldata.hs.avro\",\"symbols\":[\"BatchCalculationStarted\",\"BatchCalculationStopped\",\"Unkown\"]}");
+  BatchCalculationStarted, BatchCalculationStopped, Unknown  ;
+  public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"enum\",\"name\":\"EventBusRecordStatusAvro\",\"namespace\":\"carldata.hs.avro\",\"symbols\":[\"BatchCalculationStarted\",\"BatchCalculationStopped\",\"Unknown\"]}");
   public static org.apache.avro.Schema getClassSchema() { return SCHEMA$; }
 }
