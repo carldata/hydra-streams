4b07ac25ed41f2466340a0a05ce2018d387d35f9
Krzysztof Langner <klangner@gmail.com>
2017-10-04 09:28:03 +0200

    Improved EventBusRecord

--- src/main/scala/carldata/hs/EventBus.scala
+++ src/main/scala/carldata/hs/EventBus.scala
@@ -7,34 +7,39 @@ import carldata.hs.impl.JsonConverters._
 object EventBus {
   sealed trait Status
 
-  case object BatchCalculationStarted extends Status
-  case object BatchCalculationStopped extends Status
-  case object BatchCalculationUnknown extends Status
+  case object Started extends Status
+  case object Stopped extends Status
+  case object Other extends Status
 
-  case class EventBusRecord(calculationId: String, status: Status)
+  case class EventBusRecord(source: String, id: String, status: Status, message: String)
 
   object EventBusRecordJsonProtocol extends DefaultJsonProtocol {
 
     implicit object EventBusRecordJsonFormat extends RootJsonFormat[EventBusRecord] {
       def write(r: EventBusRecord) =
-        JsObject("calculationId" -> JsString(r.calculationId),
-          r.status match {
-            case BatchCalculationStarted => "status" -> JsString("BatchCalculationStarted")
-            case BatchCalculationStopped => "status" -> JsString("BatchCalculationStopped")
-            case BatchCalculationUnknown => "status" -> JsString("")
-          }
+        JsObject(
+          "source" -> JsString(r.source),
+          "id" -> JsString(r.id),
+          "status" -> JsString(r.status match {
+            case Started => "Started"
+            case Stopped => "Stopped"
+            case Other => ""
+          }),
+          "msg" -> JsString(r.message)
         )
 
       def read(value: JsValue): EventBusRecord = value match {
         case JsObject(fs) =>
-          val calculationId: String = fs.get("calculationId").map(stringFromValue).getOrElse("")
+          val source: String = fs.get("source").map(stringFromValue).getOrElse("")
+          val id: String = fs.get("id").map(stringFromValue).getOrElse("")
           val status: Status = fs.get("status").map {
-            case JsString("BatchCalculationStarted") => BatchCalculationStarted
-            case JsString("BatchCalculationStopped") => BatchCalculationStopped
-            case _ => BatchCalculationUnknown
-          }.getOrElse(BatchCalculationUnknown)
-          EventBusRecord(calculationId, status)
-        case _ => EventBusRecord("json-format-error", BatchCalculationUnknown)
+            case JsString("Started") => Started
+            case JsString("Stopped") => Stopped
+            case _ => Other
+          }.getOrElse(Other)
+          val msg: String = fs.get("msg").map(stringFromValue).getOrElse("")
+          EventBusRecord(source, id, status, msg)
+        case _ => EventBusRecord("Json parser", "error", Other, value.toString())
       }
     }
   }
